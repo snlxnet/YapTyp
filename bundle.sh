f3285fcb6e31b1069a6726adfdf47c04edd6f5d0
Alex <alexanderthestudent@yandex.ru>
2026-04-23 18:49:13 +0300

    feat: add YAPTYP_DIR and silence the 2nd and 3rd compiler output

--- bundle.sh
+++ bundle.sh
@@ -1,9 +1,11 @@
-typst compile --input mode=notes main.typ note{0p}.svg
-typst compile --input mode=slides main.typ slide{0p}.svg
-typst compile --input mode=article --features=html main.typ article.html
+tmp="${YAPTYP_DIR:-.}"
 
-cat head.html slide*.svg mid.html note*.svg tail.html > player.html
-rm note*.svg slide*.svg
+typst compile --input mode=notes main.typ $tmp/note{0p}.svg
+typst compile --input mode=slides main.typ $tmp/slide{0p}.svg &> /dev/null
+typst compile --input mode=article --features=html main.typ article.html &> /dev/null
+
+cat $tmp/head.html $tmp/slide*.svg $tmp/mid.html $tmp/note*.svg $tmp/tail.html > player.html
+rm $tmp/note*.svg $tmp/slide*.svg
 
 echo 'Compilation complete. Saved:'
 echo '- player.html'
