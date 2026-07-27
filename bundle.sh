acb7c330f11797f05085121563c15250d07f1eba
Alex <alexanderthestudent@yandex.ru>
2026-07-27 21:54:33 +0300

    create template.html

--- bundle.sh
+++ bundle.sh
@@ -5,4 +5,8 @@ typst compile main.typ $tmp/page{0p}.svg
 cat $tmp/head.html $tmp/page*.svg $tmp/tail.html > player.html
 rm $tmp/page*.svg
 
+cat $tmp/head.html > template.html
+echo "INSERT_SVG_HERE" >> template.html
+cat $tmp/tail.html >> template.html
+
 echo 'Compilation complete. Saved player.html'
